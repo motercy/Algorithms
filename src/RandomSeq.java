0c3dd9d5ce06d5cbfee81f5a58f1e7ad4161586b
motercy <chengyong19772003@hotmail.com>
2017-03-27 13:57:39 +0800

    RandomSeq

---
+++ src/RandomSeq.java
@@ -0,0 +1,34 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomSeq {
+	private RandomSeq() { }
+	public static void main(String[] args) {
+		int n = Integer.parseInt(args[0]);
+		
+		if (args.length == 1) {
+            // generate and print n numbers between 0.0 and 1.0
+            for (int i = 0; i < n; i++) {
+                double x = StdRandom.uniform();
+                StdOut.println(x);
+            }
+        }
+		
+		else if (args.length == 3) {
+            double lo = Double.parseDouble(args[1]);
+            double hi = Double.parseDouble(args[2]);
+
+            // generate and print n numbers between lo and hi
+            for (int i = 0; i < n; i++) {
+                double x = StdRandom.uniform(lo, hi);
+                StdOut.printf("%.2f\n", x);
+            }
+        }
+
+        else {
+            throw new IllegalArgumentException("Invalid number of arguments");
+        }		
+
+	}
+
+}
