1e2693f4327e4df831794781666f62ffca0e8fcd
motercy <chengyong19772003@hotmail.com>
2017-03-30 09:43:27 +0800

    add comments

--- src/Average.java
+++ src/Average.java
@@ -1,3 +1,21 @@
+/******************************************************************************
+ *  Compilation:  javac Average.java
+ *  Execution:    java Average < data.txt
+ *  Dependencies: StdIn.java StdOut.java
+ *  
+ *  Reads in a sequence of real numbers, and computes their average.
+ *
+ *  % java Average
+ *  10.0 5.0 6.0
+ *  3.0 7.0 32.0
+ *  [Ctrl-d]
+ *  Average is 10.5
+ *
+ *  Note [Ctrl-d] signifies the end of file on Unix.
+ *  On windows use [Ctrl-z].
+ *
+ ******************************************************************************/
+
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,3 +1,27 @@
+/******************************************************************************
+ *  Compilation:  javac BinarySearch.java
+ *  Execution:    java BinarySearch whitelist.txt < input.txt
+ *  Dependencies: In.java StdIn.java StdOut.java
+ *  Data files:   http://algs4.cs.princeton.edu/11model/tinyW.txt
+ *                http://algs4.cs.princeton.edu/11model/tinyT.txt
+ *                http://algs4.cs.princeton.edu/11model/largeW.txt
+ *                http://algs4.cs.princeton.edu/11model/largeT.txt
+ *
+ *  % java BinarySearch tinyW.txt < tinyT.txt
+ *  50
+ *  99
+ *  13
+ *
+ *  % java BinarySearch largeW.txt < largeT.txt | more
+ *  499569
+ *  984875
+ *  295754
+ *  207807
+ *  140925
+ *  161828
+ *  [367,966 total values]
+ *  
+ ******************************************************************************/
 import java.util.Arrays;
 
 import edu.princeton.cs.algs4.In;
